ad9d92ec0b0745cee4160ac9a58dc620611554d5
Jesse Berman <jesse@jesse.local>
2010-05-18 10:53:46 -0700

    added properties component

--- example-camel-logfile-streamer/src/main/java/com/example/camel/Main.java
+++ example-camel-logfile-streamer/src/main/java/com/example/camel/Main.java
@@ -1,10 +1,10 @@
 package com.example.camel;
 
-import java.util.Properties;
 import java.util.UUID;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -14,15 +14,18 @@ public class Main {
 	private static final Log log = LogFactory.getLog(Main.class);
 	
 	public static void main(String... args) throws Exception {
-		final Properties props = new Properties();
-		props.load(Main.class.getResourceAsStream("/environment.properties"));
-		
 		CamelContext context = new DefaultCamelContext();
+		
+		PropertiesComponent pc = new PropertiesComponent();
+		pc.setLocation("classpath:environment.properties");
+		
+		context.addComponent("properties", pc);
+		
 		context.addRoutes(new RouteBuilder(){
 			@Override
 			public void configure() {
 				// tails a log file and puts each line on a log queue
-				from("stream:file?fileName=" + props.getProperty("log.file") +"&scanStream=true&scanStreamDelay=1000")
+				from("stream:file?fileName={{log.file}}&scanStream=true&scanStreamDelay=1000")
 				.to("seda:logQueue");
 				
 				from("seda:logQueue")
@@ -30,6 +33,7 @@ public class Main {
 				.to("stream:out");
 			}
 		});
+		
 		context.start();
 		
 		while(true) {
