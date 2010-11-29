d7be72921777aab27c5345d6d687005b881b9ea8
jeberma <jeberma@gmail.com>
2010-11-28 16:00:57 -0800

    changed input options to json

--- example-closure/maven-plovr-plugin/src/main/java/com/example/plovr/mojo/BuildConfig.java
+++ example-closure/maven-plovr-plugin/src/main/java/com/example/plovr/mojo/BuildConfig.java
@@ -1,28 +1,182 @@
 package com.example.plovr.mojo;
 
 import java.io.File;
-import java.util.List;
+import java.io.IOException;
 
-public class BuildConfig extends Config {
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.CharEncoding;
+import org.apache.commons.lang.StringUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
+import org.plovr.CheckedSoySyntaxException;
+import org.plovr.Compilation;
+import org.plovr.CompileRequestHandler;
+import org.plovr.ConfigParser;
+import org.plovr.MissingProvideException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+import com.google.javascript.jscomp.JSError;
+
+public class BuildConfig {
+	
+	private static final String KEY_PATHS = "paths";
+	
+	private static final String KEY_INPUTS ="inputs";
+	
+	private static final String KEY_CLOSURE_LIBRARY = "closure-library";
+	
+	private static final String KEY_ID = "id";
 	
 	/**
 	 * @parameter
 	 */
-	protected File outputFile;
+	File outputFile;
 	
 	/**
 	 * @parameter
 	 */
-	protected File sourceMapFile;
+	File sourceMapFile;
 	
+	/**
+	 * @parameter
+	 */
+	File workDirectory;
 	
-	@Override
-	public List<String> validate() {
-		List<String> errors = super.validate();
-		if(outputFile == null) {
-			errors.add("outputFile is required");
+	/**
+	 * @parameter
+	 */
+	String options;
+	
+	private JsonObject json;
+	
+	private Log log;
+	
+	public void compile(Log log) throws MojoExecutionException {
+		this.log = log;
+		this.json = parseOptions();
+		sanitize();
+		validate();
+		File configFile = writeConfigFile();
+		Compilation compilation =  compile(configFile);
+		
+		logWarnings(compilation);
+		assertNoErrors(compilation);
+		writeCompiledCode(compilation);
+		writeSourceMap(compilation);
+	}
+	
+	private JsonObject parseOptions() {
+		return (JsonObject)new JsonParser().parse(options);
+	}
+	
+	private void sanitize() {
+		fixPathnames(KEY_PATHS);
+		fixPathnames(KEY_INPUTS);
+		fixPathnames(KEY_CLOSURE_LIBRARY);
+	}
+	
+	private void fixPathnames(String key) {
+		if(json.has(key)) {
+			JsonElement paths = json.get(key);
+			json.remove(key);
+			if(paths.isJsonArray()) {
+				json.add(key, fixPathnames((JsonArray)paths));
+			} else {
+				json.addProperty(key, fixPathname(paths.getAsString()));
+			}
 		}
-		return errors;
+	}
+	
+	private JsonArray fixPathnames(JsonArray input) {
+		JsonArray result = new JsonArray();
+		for(int i=0; i<input.size(); i++) {
+			result.add(
+					new JsonPrimitive(
+							fixPathname(input.get(i).getAsString())
+					)
+			);
+		}
+		return result;
+	}
+	
+	private String fixPathname(String input) {
+		return FilenameUtils.separatorsToSystem(StringUtils.trim(input));
+	}
+	
+	private void validate() throws MojoExecutionException {
+		if(!json.has(KEY_ID)) {
+			throw new MojoExecutionException("options.id is required");
+		}
+	}
+	
+	private File writeConfigFile() throws MojoExecutionException {
+		try {
+			FileUtils.forceMkdir(workDirectory);
+			File file = new File(workDirectory, json.get(KEY_ID).getAsString());
+			FileUtils.writeStringToFile(file, new Gson().toJson(json), CharEncoding.UTF_8);
+			return file;
+		} catch(IOException e) {
+			throw new MojoExecutionException("failed to write config file", e);
+		}
+	}
+	
+	private Compilation compile(File configFile) throws MojoExecutionException {
+		try {
+			return CompileRequestHandler.compile(ConfigParser.parseFile(configFile));
+		} catch(IOException e) {
+			throw new MojoExecutionException("failed to write config file", e);
+		} catch(CheckedSoySyntaxException e) {
+			throw new MojoExecutionException("compilation failed", e);
+		} catch(MissingProvideException e) {
+			throw new MojoExecutionException("compilation failed", e);
+		}
+	}
+	
+	private void logWarnings(Compilation compilation) {
+		for(JSError each : compilation.getResult().warnings) {
+			log.warn(each.toString());
+		}
+	}
+	
+	private void assertNoErrors(Compilation compilation) throws MojoExecutionException {
+		if(compilation.getResult().errors.length > 0) {
+			for(JSError each : compilation.getResult().errors) {
+				log.error(each.toString());
+			}
+			throw new MojoExecutionException(
+					"BUILD FAILED: " + compilation.getResult().errors.length + 
+					" Errors, " + compilation.getResult().warnings.length + " Warnings"
+			);
+		}
 	}
 
+	private void writeSourceMap(Compilation compilation) throws MojoExecutionException {
+		if(sourceMapFile == null) {
+			return;
+		}
+		try {
+			StringBuilder sb = new StringBuilder();
+			compilation.getResult().sourceMap.appendTo(sb, json.get(KEY_ID).getAsString());
+			FileUtils.writeStringToFile(sourceMapFile, sb.toString());
+		} catch(IOException e) {
+			throw new MojoExecutionException("failed to write source map", e);
+		}
+	}
+	
+	private void writeCompiledCode(Compilation compilation) throws MojoExecutionException {
+		if(outputFile == null) {
+			return;
+		}
+		try {
+			FileUtils.writeStringToFile(outputFile, compilation.getCompiledCode());
+		} catch(IOException e) {
+			throw new MojoExecutionException("failed to write output file", e);
+		}
+	}
 }
--- example-closure/maven-plovr-plugin/src/main/java/com/example/plovr/mojo/BuildMojo.java
+++ example-closure/maven-plovr-plugin/src/main/java/com/example/plovr/mojo/BuildMojo.java
@@ -1,18 +1,10 @@
 package com.example.plovr.mojo;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.plovr.CheckedSoySyntaxException;
-import org.plovr.Compilation;
-import org.plovr.CompileRequestHandler;
-import org.plovr.ConfigParser;
-import org.plovr.MissingProvideException;
-
-import com.google.javascript.jscomp.Result;
 
 /**
  * Goal that executes the plovr build command.
@@ -20,63 +12,27 @@ import com.google.javascript.jscomp.Result;
  * @goal build
  * @phase package
  */
-public class BuildMojo extends AbstractPlovrMojo {
+public class BuildMojo extends AbstractMojo {
 	
 	/**
 	 * @parameter
 	 */
 	private List<BuildConfig> buildConfigs;
 	
+	/**
+	 * @parameter expression="${project.build.directory}/plovr"
+	 */
+	private File workDirectory;
+	
 	public void execute() throws MojoExecutionException {
 		if(buildConfigs == null) {
 			return;
 		}
 		for(BuildConfig each : buildConfigs) {
-			setDefaults(each);
-			validate(each);
-			Compilation compilation = compile(each);
-			logWarnings(compilation.getResult());
-			assertNoErrors(compilation.getResult());
-			writeCompiledCode(each.outputFile, compilation);
-			writeSourceMap(compilation.getResult(), each);
-		}
-	}
-	
-	private Compilation compile(BuildConfig config) throws MojoExecutionException {
-		if(config.outputFile == null) {
-			throw new MojoExecutionException("no output file specified");
-		}
-		try {
-			File configFile = config.writeToFile();
-			return CompileRequestHandler.compile(ConfigParser.parseFile(configFile));
-		} catch(IOException e) {
-			throw new MojoExecutionException("failed to write config file", e);
-		} catch(CheckedSoySyntaxException e) {
-			throw new MojoExecutionException("compilation failed", e);
-		} catch(MissingProvideException e) {
-			throw new MojoExecutionException("compilation failed", e);
-		}
-	}
-	
-	private void writeCompiledCode(File outputFile, Compilation compilation) throws MojoExecutionException {
-		try {
-			FileUtils.writeStringToFile(outputFile, compilation.getCompiledCode());
-		} catch(IOException e) {
-			throw new MojoExecutionException("failed to write output file", e);
-		}
-	}
-	
-	private void writeSourceMap(Result result, BuildConfig config) throws MojoExecutionException {
-		if(config.sourceMapFile == null) {
-			return;
-		}
-		try {
-			StringBuilder sb = new StringBuilder();
-			result.sourceMap.appendTo(sb, config.id);
-			FileUtils.writeStringToFile(config.sourceMapFile, sb.toString());
-		} 
-		catch(IOException e) {
-			throw new MojoExecutionException("failed to write source map", e);
+			if(each.workDirectory == null) {
+				each.workDirectory = workDirectory;
+			}
+			each.compile(getLog());
 		}
 	}
 }
