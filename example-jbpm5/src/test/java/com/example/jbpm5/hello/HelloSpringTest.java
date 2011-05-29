622b8ba3e3d7caa35e7f12a343a788d2a20536ed
jeberma <jeberma@gmail.com>
2011-05-29 15:30:28 -0700

    added variable example

--- example-jbpm5/src/test/java/com/example/jbpm5/hello/HelloSpringTest.java
+++ example-jbpm5/src/test/java/com/example/jbpm5/hello/HelloSpringTest.java
@@ -1,5 +1,9 @@
 package com.example.jbpm5.hello;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +18,9 @@ public class HelloSpringTest extends AbstractJUnit4SpringContextTests{
 	
 	@Test
 	public void hello() {
-		//ksession.startProcess("com.example.jbpm5.hello.HelloProcess");
-		ksession.startProcess("com.example.jbpm5.hello.SpringProcess");
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", UUID.randomUUID().toString());
+		ksession.startProcess("com.example.jbpm5.hello.SpringProcess", params);
 	}
 
 }
